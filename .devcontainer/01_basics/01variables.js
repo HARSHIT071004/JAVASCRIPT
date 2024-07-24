1d4e751dff4ae2dc67121800f2ff8c43738c989b
HARSHIT071004 <harsh078976@gmail.com>
2024-07-24 15:51:49 +0000

    datatypes

---
+++ .devcontainer/01_basics/01variables.js
@@ -0,0 +1,13 @@
+const accountID= 12233
+let accountEmail = "hello@gmail.com"
+var accountpassword = "22344"
+accountCity = "jaipur"
+/*prefer not to use var instead
+ let and const*/
+ let hello;
+accountID =3   //variable ca declare i this way also
+console.log(accountID);   //cnt cannot be modify
+accountpassword = "22"
+console.log(accountpassword); //many variable can be chne by below given method
+console.table([acoountID,accountpassword,accountEmail,accountCity,hello]) //this hello is undefined
+
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS as newer version
+alert(3+3)  //it gives error as it is in nodejs not in browser or in console of google
+
+console.log("hello");   console.log("hii")   //this will not give error but it is not readable format
+let age = 18    //num is 2 to poer 53 << BigInt   <<there isbolean which returns true and false
+let name = "hello"
+let isLoggedIn = false
+let state = null
+// objects below
+console.log(typeof age);
+console.log(typeof age);
+console.log(typeof null);   //here type is objects
+console.log(typeof undefined); //type is undefined 
+ 
+
+
+
+
+
