7d89655663c2ef195bdf64e9232cedc53a066ab4
HARSHIT071004 <harsh078976@gmail.com>
2024-07-25 12:09:00 +0000

    conversion

---
+++ .devcontainer/01_basics/01_coversiondatatype.js
@@ -0,0 +1,24 @@
+let score =2
+let age="33abc"
+let name = null
+let hello= undefined
+let bool=true
+let value = Number(age)
+let value1 = Number(score)
+let value2 = Number(name)  
+let value3 = Number(hello) 
+let value4 = Number(bool) 
+console.log(typeof score); 
+console.log(typeof age);//it is basically type converesion 
+console.log(typeof value); //this is used toc heck type of given age which output is NaN
+// above is basically problem in JS     
+console.log(typeof name);   // prints 0
+console.log(typeof hello);  //prints NaN 
+console.log(typeof bool); 
+let hii="hitesh"
+let boole = Boolean(hii)
+console.log(boole);   //this will give true but for empty string it will give false
+let hell=33
+let stringnum = String(hell)
+console.log(stringnum);
+console.log(typeof stringnum);
