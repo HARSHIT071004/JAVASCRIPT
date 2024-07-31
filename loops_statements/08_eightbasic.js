007f6a186b3ba9f5e38f3abc9bd501e50803d433
HARSHIT071004 <harsh078976@gmail.com>
2024-07-31 12:05:18 +0000

    jidn di mili

--- loops_statements/08_eightbasic.js
+++ loops_statements/08_eightbasic.js
@@ -1,15 +1,15 @@
 const myNums = [1, 2, 3]
-
-// const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
-//     return acc + currval
-// }, 0)
-
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+// 1
+// const myTotal = myNums.reduce(function (acc, currval) {    >>>>here recuce function is used 
+//     console.log(`acc: ${acc} and currval: ${currval}`);       //har baar ki value print here in acumulator if u print 3 then first line input is 3                                                                                                                                                                                         
+//     return acc + currval                              >>>>it will print sum of funal value
+// }, 0)                                                 >>>>>>this is start value of accumulator
+// 2
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)    //>>it is same code as given above but i smart way      //>>>>>here arrow fumnction is used here 0 is start value
 
 console.log(myTotal);
 
-
+//3 
 const shoppingCart = [
     {
         itemName: "js course",
@@ -29,6 +29,6 @@ const shoppingCart = [
     },
 ]
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)    // it is basically a use of reduce function
 
-console.log(priceToPay);
+console.log(priceToPay);    //output is 22996
